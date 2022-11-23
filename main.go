5b7719db22be6477d871b3b923014052673e78da
322rxm <jeff.wuestling@gmail.com>
2022-11-23 13:01:17 -0500

    added geocode api key

--- main.go
+++ main.go
@@ -338,7 +338,7 @@ func GetTaxiTrips(db *sql.DB) {
 	// Get your geocoder.ApiKey from here :
 	// https://developers.google.com/maps/documentation/geocoding/get-api-key?authuser=2
 
-	geocoder.ApiKey = "ADD_YOUR_API_KEY_HERE"
+	geocoder.ApiKey = "AIzaSyAcbBherGHzPZMJMwjV-C4LlfanMkUq7lE"
 
 	drop_table := `drop table if exists taxi_trips`
 	_, err := db.Exec(drop_table)
