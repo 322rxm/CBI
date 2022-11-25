8c0c0e2a2f257310a7047544170a60d6e3869701
322rxm <jeff.wuestling@gmail.com>
2022-11-25 10:46:19 -0500

    troubleshooting COVID details table load

--- main.go
+++ main.go
@@ -1246,6 +1246,7 @@ func GetCovidDetails(db *sql.DB) {
 			if zip_code == "" {
 				continue
 			}
+			fmt.Println(zip_code)
 		// 2
 			week_number := covid_details_list[i].Week_number
 			if week_number == "" {
