442f417163f9dec11fd54c9ed75bfe72ca8b6da1
322rxm <jeff.wuestling@gmail.com>
2022-11-25 11:15:44 -0500

    take out data validation for COVID details table

--- main.go
+++ main.go
@@ -1241,115 +1241,115 @@ func GetCovidDetails(db *sql.DB) {
 
 	for i := 0; i < len(covid_details_list); i++ {
 
-		// 1
-			zip_code := covid_details_list[i].Zip_code
-			if zip_code == "" {
-				continue
-			}
-			fmt.Println(zip_code)
-		// 2
-			week_number := covid_details_list[i].Week_number
-			if week_number == "" {
-				continue
-			}
-		// 3	
-			week_start := covid_details_list[i].Week_start
-			if week_start == "" {
-				continue
-			}
-		// 4	
-			week_end := covid_details_list[i].Week_end
-			if week_end == "" {
-				continue
-			}
-		// 5	
-			cases_weekly := covid_details_list[i].Cases_weekly
-			if cases_weekly == "" {
-				continue
-			}
-		// 6
-			cases_cumulative := covid_details_list[i].Cases_cumulative
-			if cases_cumulative == "" {
-				continue
-			}
-		// 7	
-			case_rate_weekly := covid_details_list[i].Case_rate_weekly
-			if case_rate_weekly == "" {
-				continue
-			}
-		// 8	
-			case_rate_cumulative := covid_details_list[i].Case_rate_cumulative
-			if case_rate_cumulative == "" {
-				continue
-			}
-		// 9	
-			tests_weekly := covid_details_list[i].Tests_weekly
-			if tests_weekly == "" {
-				continue
-			}
-		// 10	
-			tests_cumulative := covid_details_list[i].Tests_cumulative
-			if tests_cumulative == "" {
-				continue
-			}
-		// 11	
-			test_rate_weekly := covid_details_list[i].Test_rate_weekly
-			if test_rate_weekly == "" {
-				continue
-			}
-		// 12	
-			test_rate_cumulative := covid_details_list[i].Test_rate_cumulative
-			if test_rate_cumulative == "" {
-				continue
-			}
-		// 13	
-			percent_tested_positive_weekly := covid_details_list[i].Percent_tested_positive_weekly
-			if percent_tested_positive_weekly == "" {
-				continue
-			}
-		// 14	
-			percent_tested_positive_cumulative := covid_details_list[i].Percent_tested_positive_cumulative
-			if percent_tested_positive_cumulative == "" {
-				continue
-			}
-		// 15	
-			deaths_weekly := covid_details_list[i].Deaths_weekly
-			if deaths_weekly == "" {
-				continue
-			}
-		// 16	
-			deaths_cumulative := covid_details_list[i].Deaths_cumulative
-			if deaths_cumulative == "" {
-				continue
-			}
-		// 17	
-			death_rate_weekly := covid_details_list[i].Death_rate_weekly
-			if death_rate_weekly == "" {
-				continue
-			}
-		// 18	
-			death_rate_cumulative := covid_details_list[i].Death_rate_cumulative
-			if death_rate_cumulative == "" {
-				continue
-			}
-		// 19	
-			population := covid_details_list[i].Population
-			if population == "" {
-				continue
-			}
-		// 20	
-			row_id := covid_details_list[i].Row_id
-			if row_id == "" {
-				continue
-			}
-		// 21	
-			zip_code_location := covid_details_list[i].Zip_code_location
-			if zip_code_location == "" {
-				continue
-			}
+		// // 1
+		// 	zip_code := covid_details_list[i].Zip_code
+		// 	if zip_code == "" {
+		// 		continue
+		// 	}
+		// 	fmt.Println(zip_code)
+		// // 2
+		// 	week_number := covid_details_list[i].Week_number
+		// 	if week_number == "" {
+		// 		continue
+		// 	}
+		// // 3	
+		// 	week_start := covid_details_list[i].Week_start
+		// 	if week_start == "" {
+		// 		continue
+		// 	}
+		// // 4	
+		// 	week_end := covid_details_list[i].Week_end
+		// 	if week_end == "" {
+		// 		continue
+		// 	}
+		// // 5	
+		// 	cases_weekly := covid_details_list[i].Cases_weekly
+		// 	if cases_weekly == "" {
+		// 		continue
+		// 	}
+		// // 6
+		// 	cases_cumulative := covid_details_list[i].Cases_cumulative
+		// 	if cases_cumulative == "" {
+		// 		continue
+		// 	}
+		// // 7	
+		// 	case_rate_weekly := covid_details_list[i].Case_rate_weekly
+		// 	if case_rate_weekly == "" {
+		// 		continue
+		// 	}
+		// // 8	
+		// 	case_rate_cumulative := covid_details_list[i].Case_rate_cumulative
+		// 	if case_rate_cumulative == "" {
+		// 		continue
+		// 	}
+		// // 9	
+		// 	tests_weekly := covid_details_list[i].Tests_weekly
+		// 	if tests_weekly == "" {
+		// 		continue
+		// 	}
+		// // 10	
+		// 	tests_cumulative := covid_details_list[i].Tests_cumulative
+		// 	if tests_cumulative == "" {
+		// 		continue
+		// 	}
+		// // 11	
+		// 	test_rate_weekly := covid_details_list[i].Test_rate_weekly
+		// 	if test_rate_weekly == "" {
+		// 		continue
+		// 	}
+		// // 12	
+		// 	test_rate_cumulative := covid_details_list[i].Test_rate_cumulative
+		// 	if test_rate_cumulative == "" {
+		// 		continue
+		// 	}
+		// // 13	
+		// 	percent_tested_positive_weekly := covid_details_list[i].Percent_tested_positive_weekly
+		// 	if percent_tested_positive_weekly == "" {
+		// 		continue
+		// 	}
+		// // 14	
+		// 	percent_tested_positive_cumulative := covid_details_list[i].Percent_tested_positive_cumulative
+		// 	if percent_tested_positive_cumulative == "" {
+		// 		continue
+		// 	}
+		// // 15	
+		// 	deaths_weekly := covid_details_list[i].Deaths_weekly
+		// 	if deaths_weekly == "" {
+		// 		continue
+		// 	}
+		// // 16	
+		// 	deaths_cumulative := covid_details_list[i].Deaths_cumulative
+		// 	if deaths_cumulative == "" {
+		// 		continue
+		// 	}
+		// // 17	
+		// 	death_rate_weekly := covid_details_list[i].Death_rate_weekly
+		// 	if death_rate_weekly == "" {
+		// 		continue
+		// 	}
+		// // 18	
+		// 	death_rate_cumulative := covid_details_list[i].Death_rate_cumulative
+		// 	if death_rate_cumulative == "" {
+		// 		continue
+		// 	}
+		// // 19	
+		// 	population := covid_details_list[i].Population
+		// 	if population == "" {
+		// 		continue
+		// 	}
+		// // 20	
+		// 	row_id := covid_details_list[i].Row_id
+		// 	if row_id == "" {
+		// 		continue
+		// 	}
+		// // 21	
+		// 	zip_code_location := covid_details_list[i].Zip_code_location
+		// 	if zip_code_location == "" {
+		// 		continue
+		// 	}
 		
-			fmt.Println(zip_code_location)
-			
+			//fmt.Println(zip_code_location)
+
 			sql := `INSERT INTO covid_details ("zip_code,"
 			"week_number,"
 			"week_start,"
